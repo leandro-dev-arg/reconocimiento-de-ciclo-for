2c12d51dbba845f352aedf75d0c9debcfb0dcdda
leandro <leandro.dev.arg@gmail.com>
2023-12-18 12:39:40 -0300

    primer subida,unidad 5-ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace U5_programa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int x = 0; x < 10; x++)
+            {
+             Console.WriteLine(x+1);
+            }
+
+        }
+    }
+}
